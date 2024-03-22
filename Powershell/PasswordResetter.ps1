54f206bac4e492d5c3d17c9294411f6b31b3b11a
MoebiusZero <53936928+MoebiusZero@users.noreply.github.com>
2024-03-22 10:58:41 +0100

    Create PasswordResetter.ps1
    Script to mass password change specific accounts that start with a certain name.
    
    Great for mass resetting multiple service accounts used for a application

---
+++ Powershell/PasswordResetter.ps1
@@ -0,0 +1,44 @@
+#Check if the module is installed
+$checkmodule = Get-Module -Name ActiveDirectory
+if ($checkmodule -ne $null) {
+    Write-Host "Module installed, continuing..." 
+} else {
+    Write-Host "This Module requires the ActiveDirectory module to get AD users and change passwords, please install it first or run this on a jump server with the Active Directory module" 
+} 
+
+#Get all accounts
+$accounts = Get-ADUser -filter "samaccountname -like 'Synergy.*'"
+
+#Change these parameters to alter the sender/recipients/contents of the E-mail
+$from = 'Synergy Password Resetter <ict.support@nijhuisindustries.com>'
+
+#Loop through each account and change the password
+foreach ($account in $accounts) { 
+    
+    #Generate a new password
+    $number = Get-Random -Minimum 1000 -Maximum 9999
+    $password = "Highdidleydoo$number!" 
+
+    #Set the password
+    Set-ADAccountPassword -Identity $account.SamAccountName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)   
+
+    #Set the To, CC and BCC for each synergy account
+    switch ($account.name) {
+        "Account 1" { 
+			#"<e-mail>" for single addresses, @("<email 1>", "<email 2>") for multiple
+            $to = "<e-mail>"
+            $cc = @("<e-mail>")
+            $bcc = @("<e-mail>")
+        }           
+         default { 
+            $to = "<e-mail>"
+            $cc = @("<e-mail>")
+            $bcc = @("<e-mail>")
+         }
+    }
+    
+    #Set the e-mail subject for this account    
+    $subject = $Account.name + " password has been reset" 
+    $body = "<h2>Password has been reset for " + $account.name + "</h2><br><b>Username: </b>" + $account.samaccountname + "<br><b>Password: </b>$password<br><br>With regards,<br>Your Friendly Neighbourhood IT Department"
+    Send-MailMessage -to $to -cc $cc -bcc $bcc -Subject $subject -from $from -Body $body -SmtpServer "<mail-server>" -BodyAsHtml
+} 
